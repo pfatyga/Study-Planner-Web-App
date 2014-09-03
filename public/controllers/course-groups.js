e0efb13b90523a8f37bfa9fce80645f85a2c832f
Matt Continisio <mrcontinisio@gmail.com>
2014-09-03 15:32:52 -0400

    improved course group forms

--- public/controllers/course-groups.js
+++ public/controllers/course-groups.js
@@ -1,3 +1,25 @@
 angular.module('MyApp')
 	.controller('CourseGroupsController', ['$scope', function($scope) {
+		// TODO - load from database
+		$scope.courseGroups = [
+			{
+				name: "Computer Science",
+				courses: []
+			},
+			{
+				name: "Technical Electives",
+				courses: []
+			},
+			{
+				name: "Software Development Electives",
+				courses: []
+			},
+		];
+
+		$scope.deleteModal = {
+		};
+
+		$scope.deleteCourseGroup = function() {
+			confirm('Are you sure?');
+		};
 	}]);
