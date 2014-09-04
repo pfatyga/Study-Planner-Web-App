000cc314f970215f12a5c4b465d8d330a3098284
Matt Continisio <mrcontinisio@gmail.com>
2014-09-04 12:49:48 -0400

    put degrees in controller, view loads from controller

--- public/controllers/admin/admin-degrees.js
+++ public/controllers/admin/admin-degrees.js
@@ -1,5 +1,21 @@
 angular.module('MyApp')
 	.controller('AdminDegreesController', ['$scope', function($scope) {
+		// TODO - use databse
+		$scope.degrees = [
+			{
+				id: 0,
+				name: 'Computer Science (CS)',
+				startingWith: 'CS 115',
+				semester: 'Fall 2015'
+			},
+			{
+				id: 1,
+				name: 'Cybersecurity (CyS)',
+				startingWith: 'CS 105',
+				semester: 'Fall 2015'
+			}
+		];
+
 		$scope.deleteModal = {
 			title: "Delete Degree",
 			content: "Are you sure you want to delete this degree?"
