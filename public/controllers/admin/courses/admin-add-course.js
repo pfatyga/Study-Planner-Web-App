bba4553653ecb951a58158b3e7c806607ba60a11
Matt Continisio <mrcontinisio@gmail.com>
2014-09-04 15:20:58 -0400

    improved add course form styling

--- public/controllers/admin/courses/admin-add-course.js
+++ public/controllers/admin/courses/admin-add-course.js
@@ -1,14 +1,31 @@
 angular.module('MyApp')
 	.controller('AdminAddCourseController', ['$scope', function($scope) {
-		console.log("AdminAddCourseController controller running");
-		$scope.preReqs = 
+		//console.log("AdminAddCourseController controller running");
+		$scope.preReqs = [
 			[
-				[{name:""}]
-			];
-		$scope.preReqAnd = function () {
-			$scope.preReqs.push([{name: ""}]);
+				{ name: "" }
+			]
+		];
+
+		$scope.preReqAnd = function() {
+			$scope.preReqs.push([{ name: "" }]);
 		};
-		$scope.preReqOr = function (index) {
-			$scope.preReqs[index].push({name: ""});
+
+		$scope.preReqOr = function(index) {
+			$scope.preReqs[index].push({ name: "" });
+		};
+
+		$scope.coReqs = [
+			[
+				{ name: "" }
+			]
+		];
+
+		$scope.coReqAnd = function() {
+			$scope.coReqs.push([{ name: "" }]);
+		};
+
+		$scope.coReqOr = function(index) {
+			$scope.coReqs[index].push({ name: "" });
 		};
 	}]);
--- public/controllers/admin/courses/admin-edit-course.js
+++ public/controllers/admin/courses/admin-edit-course.js
@@ -1,4 +1,32 @@
 angular.module('MyApp')
 	.controller('AdminEditCourseController', ['$scope', '$stateParams', function($scope, $stateParams) {
+		//console.log("AdminAddCourseController controller running");
 		console.log('courseId: ' + $stateParams.courseGroupId);
+		$scope.preReqs = [
+			[
+				{ name: "" }
+			]
+		];
+
+		$scope.preReqAnd = function() {
+			$scope.preReqs.push([{ name: "" }]);
+		};
+
+		$scope.preReqOr = function(index) {
+			$scope.preReqs[index].push({ name: "" });
+		};
+
+		$scope.coReqs = [
+			[
+				{ name: "" }
+			]
+		];
+
+		$scope.coReqAnd = function() {
+			$scope.coReqs.push([{ name: "" }]);
+		};
+
+		$scope.coReqOr = function(index) {
+			$scope.coReqs[index].push({ name: "" });
+		};
 	}]);
