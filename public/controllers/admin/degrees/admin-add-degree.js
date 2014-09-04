bb5bf2f698fae45c3b9a9b0a12f21fc2a73cfcfe
Peter Fatyga <pete0145@gmail.com>
2014-09-04 15:20:12 -0400

    Implemented the PreReq editor in the degree form

--- public/controllers/admin/degrees/admin-add-degree.js
+++ public/controllers/admin/degrees/admin-add-degree.js
@@ -1,3 +1,14 @@
 angular.module('MyApp')
 	.controller('AdminAddDegreeController', ['$scope', function($scope) {
+		console.log("AdminAddDegreeController is running");
+		$scope.reqs = 
+			[
+				[{name:""}]
+			];
+		$scope.reqAnd = function () {
+			$scope.reqs.push([{name: ""}]);
+		};
+		$scope.reqOr = function (index) {
+			$scope.reqs[index].push({name: ""});
+		};
 	}]);
