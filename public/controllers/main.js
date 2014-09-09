10cc72e349b40834a7f38efba2b61c65b7b190c0
Matt Continisio <mrcontinisio@gmail.com>
2014-09-09 17:23:11 -0400

    added Course, CourseGroup, and Degree classes

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -1,5 +1,6 @@
 angular.module('MyApp')
 	.controller('MainController', ['$scope', '$rootScope', function($scope, $rootScope) {
+		// TODO - test code, should remove
 		$rootScope.currentUser = {
 			username: 'msmith'
 		};
