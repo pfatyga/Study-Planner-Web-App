a244e9ea0bf995f03520b0f8a4e137a869dacd32
Peter Fatyga <pete0145@gmail.com>
2014-09-09 18:44:39 -0400

    Logic for adding to data structure and display data structure have been corrected.

--- public/controllers/admin/courses/admin-add-course-peter.js
+++ public/controllers/admin/courses/admin-add-course-peter.js
@@ -6,19 +6,7 @@ angular.module('MyApp')
 				[	//this array element is OR'ed
 					[	//this array element is AND'ed
 						{ name: "CS 181" }	//this array element is OR'ed
-					],
-					[
-						{ name: "CS 192" }
 					]
-				],
-				[
-					[
-						{ name: "CS 123" }, {name: "CS 543" }
-					],
-					[
-						{ name: "CS 865" }, {name: "CS 235" }
-					]
-
 				]
 			]
 		];
@@ -28,15 +16,15 @@ angular.module('MyApp')
 		};
 
 		$scope.innerAnd = function(index) {
-			$scope.preReqs[index].push({ name: "" });
+			this.$parent.preReqSubGroup.push([{name: ""}]);
 		};
 
 		$scope.outerOr = function() {
-			$scope.preReqs.push([{ name: "" }]);
+			this.$parent.preReqGroup.push([[{name: ""}]]);
 		};
 
 		$scope.outerAnd = function(index) {
-			$scope.preReqs[index].push({ name: "" });
+			this.$parent.preReqs.push([[[{name: ""}]]]);
 		};
 
 		$scope.coReqs = [
