54c2994a43fc147fb347694fdebb0fe926683841
Matt Continisio <mrcontinisio@gmail.com>
2014-09-14 18:04:44 -0400

    moved code from AdminDegreesController to AdminDegreeListController

--- public/controllers/admin/admin-degrees.js
+++ public/controllers/admin/admin-degrees.js
@@ -1,11 +1,3 @@
 angular.module('MyApp')
 	.controller('AdminDegreesController', ['$scope', 'DegreeManager', function($scope, DegreeManager) {
-		// Get degrees from DegreeManager
-		$scope.degrees = DegreeManager.getDegrees();
-
-		// Model for delete degree modal
-		$scope.deleteModal = {
-			title: "Delete Degree",
-			content: "Are you sure you want to delete this degree?"
-		};
 	}]);
