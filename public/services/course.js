043e87de0a62e9f2ba3713d270c204a5d90d7bd6
Matt Continisio <mrcontinisio@gmail.com>
2014-09-14 01:56:18 -0400

    added Course service for managing courses

---
+++ public/services/course.js
@@ -0,0 +1,107 @@
+angular.module('MyApp')
+	.factory('Course', [function($http, $location, $rootScope, $cookieStore, $alert) {
+		return {
+			// Test courses, should be loaded from a database
+			courses: [
+				{
+					id: 0,
+					prefix: "CS",
+					number: 105,
+					name: "Introduction to Scientific Computing",
+					preRequisites: [
+					],
+					coRequisites: [
+					],
+					credits: 3.0,
+					offeredFallOnCampus: true,
+					offeredSpringOnCampus: true,
+					offeredSummerOneOnCampus: false,
+					offeredSummerTwoOnCampus: false,
+					offeredFallWebCampus: false,
+					offeredSpringWebCampus: false,
+					offeredSummerOneWebCampus: false,
+					offeredSummerTwoWebCampus: false,
+				},
+				{
+					id: 1,
+					prefix: "CS",
+					number: 110,
+					name: "Creative Problem Solving with Computing",
+					preRequisites: [
+					],
+					coRequisites: [
+					],
+					credits: 3.0,
+					offeredFallOnCampus: true,
+					offeredSpringOnCampus: false,
+					offeredSummerOneOnCampus: false,
+					offeredSummerTwoOnCampus: false,
+					offeredFallWebCampus: false,
+					offeredSpringWebCampus: false,
+					offeredSummerOneWebCampus: false,
+					offeredSummerTwoWebCampus: false,
+				},
+				{
+					id: 2,
+					prefix: "CS",
+					number: 115,
+					name: "Introduction to Computer Science",
+					preRequisites: [
+					],
+					coRequisites: [
+					],
+					credits: 3.0,
+					offeredFallOnCampus: true,
+					offeredSpringOnCampus: true,
+					offeredSummerOneOnCampus: false,
+					offeredSummerTwoOnCampus: false,
+					offeredFallWebCampus: false,
+					offeredSpringWebCampus: false,
+					offeredSummerOneWebCampus: false,
+					offeredSummerTwoWebCampus: false,
+				},
+				{
+					id: 3,
+					prefix: "CS",
+					number: 284,
+					name: "Data Stuctures",
+					preRequisites: [
+						"CS 115"
+					],
+					coRequisites: [
+						"CS 135"
+					],
+					credits: 3.0,
+					offeredFallOnCampus: true,
+					offeredSpringOnCampus: true,
+					offeredSummerOneOnCampus: false,
+					offeredSummerTwoOnCampus: false,
+					offeredFallWebCampus: false,
+					offeredSpringWebCampus: false,
+					offeredSummerOneWebCampus: false,
+					offeredSummerTwoWebCampus: false,
+				}
+			],
+
+			getCourses: function() {
+				return this.courses;
+			},
+
+			getCourse: function(courseId) {
+				// TODO - error checking
+				return this.courses[courseId];
+			},
+
+			addCourse: function(course) {
+				// TODO
+			},
+
+			editCourse: function(course) {
+				// TODO
+			},
+
+			deleteCourse: function(course) {
+				// TODO
+			}
+		};
+	}]);
