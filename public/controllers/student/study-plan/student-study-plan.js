ba054ac16bf215005f4a051e732b5984e2f0bfe8
Peter Fatyga <pete0145@gmail.com>
2014-10-22 18:57:55 -0400

    Worked on study plan form

--- public/controllers/student/study-plan/student-study-plan.js
+++ public/controllers/student/study-plan/student-study-plan.js
@@ -1,8 +1,27 @@
 angular.module('MyApp')
     .controller('StudentStudyPlanController', ['$scope', "DegreeManager", function($scope, DegreeManager) {
         $scope.title = "Study Plan Builder";
-        $scope.submitText = "Generate Study Plan"
+        $scope.submitText = "Generate Study Plan";
 
         $scope.degrees = DegreeManager.getDegrees();
-        $scope.selectedDegree = $scope.degrees[0];
+        $scope.selectedDegree = $scope.degrees[0].name;
+
+        $scope.semesters = [
+            "Spring",
+            /* "Summer", */
+            "Fall"
+        ]
+        $scope.selectedSemester = $scope.semesters[0];
+
+        $scope.years = [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022
+        ]
+        $scope.selectedYear = $scope.years[0];
     }]);
